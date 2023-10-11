0b5493836d096802e0f763eec7b2d06ef860876d
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-10-11 15:53:28 -0700

    Update and rename Code_interpreter_with_E2B to Code_interpreter_with_E2B.py

--- Code_interpreter_with_E2B.py
+++ Code_interpreter_with_E2B.py
@@ -56,6 +56,29 @@ functions = [
 
 # PARSE GPT RESPONSE
 
+
+# long-running sessions
+async def run_code(code: str): 
+  global session
+  # 1. First we need to write the code to a file.
+  await session.filesystem.write("/home/user/index.js", code)
+  # 2. Then execute the file with Node.
+  proc = await session.process.start("node /home/user/index.js")
+  # 3. Wait for the process to finish.
+  out = await proc
+  # 4. Return the stdout and stderr.
+  return out.stdout, out.stderr
+
+# long-running sessions 
+async def install_package(package: str): 
+  global session
+  # 1. Install the package through starting the `npm` process
+  proc = await session.process.start(f"npm install {package}")
+  # 2. Wait for the process to finish.
+  out = await proc
+  #. 3. Return the stdout and stderr.
+  return out.stdout, out.stderr
+
 async def parse_gpt_response(response):
 message = response["choices"][0]["message"]
 if (func := message.get("function_call")):
@@ -70,17 +93,19 @@ if (func := message.get("function_call")):
   # If the model is calling the exec_code function we defined in the `functions` variable, we want to save the `code` argument to a variable.
   if func_name == "exec_code":
     code = func_args["code"]
-
-# EXECUTE THE CODE USING E2B
-  stdout, stderr = await e2b.run_code("Node16", code) 
-  print(stdout) 
-  print(stderr) 
-elif func_name == "install_package": 
-  package_name = func_args["name"] 
-else:
-  # The model didn't call a function, so we just print the message.
-  content = message["content"]
-  print(content)
+  # EXECUTE THE CODE USING E2B
+    stdout, stderr = await run_code(code) 
+    print(stdout) 
+    print(stderr) 
+  elif func_name == "install_package": 
+    package_name = func_args["name"] 
+    stdout, stderr = await install_package(package_name) 
+    print(stdout) 
+    print(stderr) 
+  else:
+    # The model didn't call a function, so we just print the message.
+    content = message["content"]
+    print(content)
 
 #############################################################################################################################################
 
